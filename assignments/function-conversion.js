fae02a79e16d2aab06b2d6e3336c6242e5815df5
barkern89 <barkern89@gmail.com>
2019-10-02 20:16:09 -0700

    finished arrays challenge 3

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,16 +5,27 @@
 // };
 // myFunction();
 
+let myFunction = () => {
+    console.log('Function was invoked!');
+}
+myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
 
+let anotherFunction = (param) =>
+    param;
+    console.log(anotherFunction("Example"));
+
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
-
+let add = (param1, param2) =>
+    param1 + param2;
+    console.log(add(1,2));
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
